0be2106c7930708aaceb0973bec6fff5fab1b040
PAULA\Paula <paula.esteban.alonso@alumnos.upm.es>
2019-06-26 13:20:19 +0200

    Más pruebas, ahora con response entity

--- src/main/java/com/controller/Main.java
+++ src/main/java/com/controller/Main.java
@@ -4,6 +4,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Scope;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,24 +17,29 @@ import com.analizador.Analizador;
 @SpringBootApplication
 public class Main {
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Main.class, args);
+	}
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-  
-  @Scope("request")
-  @RequestMapping("/checkRules/1")
-  @ResponseBody
-  public String comprobarPasiva(HttpServletResponse response,
-      @RequestParam(name="texto", required=true) String texto) throws Throwable {
-    //https://analizadorlecturafacil.herokuapp.com/analyzer?texto="holaaa" ASI ES COMO SE HACE LA LLAMADA
-    //https://analizadorlecturafacil.herokuapp.com/analyzer?texto="holaaa"&regla="1" PARA MANDAR MÁS DE UN 1 
-	  Analizador analyzer=new Analizador(texto);
-	  String resultado= analyzer.reglaPasiva(texto);	  
-	  return resultado;
-  }
+	@RequestMapping("/")
+	String index() {
+		return "index";
+	}
+
+	@Scope("request")
+	@RequestMapping("/checkRules/1")
+	@ResponseBody
+	public ResponseEntity<String> comprobarPasiva(HttpServletResponse response,
+			@RequestParam(name="texto", required=true) String texto) throws Throwable {
+		//https://analizadorlecturafacil.herokuapp.com/analyzer?texto="holaaa" ASI ES COMO SE HACE LA LLAMADA
+		//https://analizadorlecturafacil.herokuapp.com/analyzer?texto="holaaa"&regla="1" PARA MANDAR MÁS DE UN 1 
+		Analizador analyzer=new Analizador(texto);
+		String ruleJson= analyzer.reglaPasiva(texto);	
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "application/json;charset=UTF-8");
+
+
+		return new ResponseEntity<>(ruleJson, headers, HttpStatus.OK);
+		
+	}
 }
