278b8ae90b558c91b38fa1139d598e3536c51da9
PAULA\Paula <paula.esteban.alonso@alumnos.upm.es>
2019-07-03 01:56:10 +0200

    Quitando basura

--- src/main/java/com/controller/Main.java
+++ src/main/java/com/controller/Main.java
@@ -1,13 +1,9 @@
 package com.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,10 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.analizador.Analizador;
 import com.analizador.Rule;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 
 @Controller
 @SpringBootApplication
